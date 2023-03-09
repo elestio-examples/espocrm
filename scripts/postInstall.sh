96ed8c4ecce206ae42ff3f5edaf6a26893a10fb5
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-09 14:46:35 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,9 @@ set -o allexport; source .env; set +o allexport;
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 30s;
+
+sed -i "s|'outboundEmailFromAddress' => '',|'outboundEmailFromAddress' => '${SMTP_FROM_EMAIL}',|g" ./espocrm/data/config.php
+sed -i "s|'smtpServer' => '',|'smtpServer' => '${SMTP_HOST}',|g" ./espocrm/data/config.php
+sed -i "s|'smtpPort' => 587,|'smtpPort' => ${SMTP_PORT},|g" ./espocrm/data/config.php
+sed -i "s|'smtpAuth' => true,|'smtpAuth' => false,|g" ./espocrm/data/config.php
+sed -i "s|'smtpSecurity' => 'TLS',|'smtpSecurity' => '',|g" ./espocrm/data/config.php
