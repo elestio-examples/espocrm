3072a27278475d0ea4471e6a4d4c95890a4aaa5e
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-18 11:28:15 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,8 +5,21 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 90s;
 
-sed -i "s|'outboundEmailFromAddress' => '',|'outboundEmailFromAddress' => '${SMTP_FROM_EMAIL}',|g" ./espocrm/data/config.php
-sed -i "s|'smtpServer' => '',|'smtpServer' => '${SMTP_HOST}',|g" ./espocrm/data/config.php
-sed -i "s|'smtpPort' => 587,|'smtpPort' => ${SMTP_PORT},|g" ./espocrm/data/config.php
-sed -i "s|'smtpAuth' => true,|'smtpAuth' => false,|g" ./espocrm/data/config.php
-sed -i "s|'smtpSecurity' => 'TLS',|'smtpSecurity' => '',|g" ./espocrm/data/config.php
+
+if [ -e "./initialized" ]; then
+    echo "Already initialized, skipping..."
+else
+    sed -i "s|'outboundEmailFromAddress' => '',|'outboundEmailFromAddress' => '${SMTP_FROM_EMAIL}',|g" ./espocrm/data/config.php
+    sed -i "s|'smtpServer' => '',|'smtpServer' => '${SMTP_HOST}',|g" ./espocrm/data/config.php
+    sed -i "s|'smtpPort' => 587,|'smtpPort' => ${SMTP_PORT},|g" ./espocrm/data/config.php
+    sed -i "s|'smtpAuth' => true,|'smtpAuth' => false,|g" ./espocrm/data/config.php
+    sed -i "s|'smtpSecurity' => 'TLS',|'smtpSecurity' => '',|g" ./espocrm/data/config.php
+    sed -i '
+        :a
+        $!{N;ba;}
+        s/\(.*\)\(}\)/\1  location \/wss {\n    proxy_pass http:\/\/172.17.0.1:8781;\n    proxy_http_version 1.1;\n    proxy_set_header Upgrade $http_upgrade;\n    proxy_set_header Connection $connection_upgrade;\n  }\n\2/
+        ' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
+    docker exec elestio-nginx nginx -s reload;
+
+    touch "./initialized"
+fi
