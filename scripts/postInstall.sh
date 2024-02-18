f93b12085141f628537cd919addbdcf2868dab1a
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-18 11:36:40 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -6,9 +6,6 @@ echo "Waiting for software to be ready ..."
 sleep 90s;
 
 
-if [ -e "./initialized" ]; then
-    echo "Already initialized, skipping..."
-else
     sed -i "s|'outboundEmailFromAddress' => '',|'outboundEmailFromAddress' => '${SMTP_FROM_EMAIL}',|g" ./espocrm/data/config.php
     sed -i "s|'smtpServer' => '',|'smtpServer' => '${SMTP_HOST}',|g" ./espocrm/data/config.php
     sed -i "s|'smtpPort' => 587,|'smtpPort' => ${SMTP_PORT},|g" ./espocrm/data/config.php
@@ -20,6 +17,3 @@ else
         s/\(.*\)\(}\)/\1  location \/wss {\n    proxy_pass http:\/\/172.17.0.1:8781;\n    proxy_http_version 1.1;\n    proxy_set_header Upgrade $http_upgrade;\n    proxy_set_header Connection $connection_upgrade;\n  }\n\2/
         ' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
     docker exec elestio-nginx nginx -s reload;
-
-    touch "./initialized"
-fi
